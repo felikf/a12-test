417d063e0f1e7773262f1e899bfc61df09a2f959
Felix Fatka <felix.fatka@gmail.com>
2021-10-21 15:24:30 +0200

    test fixes
    https://github.com/wallabyjs/public/issues/2840

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('a12-test');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('a12-test app is running!');
-  });
 });
